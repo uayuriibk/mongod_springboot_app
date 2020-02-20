c6c4ff866b02c800d4f7c9f7f32c98a775a70201
yuriybk <email2yurii@gmail.com>
2020-02-20 23:49:47 +0200

    spring boot starter skeleton

---
+++ src/main/java/com/yuriib/springboot/mongod_springboot_app/MongodSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.mongod_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodSpringbootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodSpringbootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuriib/springboot/mongod_springboot_app/MongodSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.mongod_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodSpringbootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
