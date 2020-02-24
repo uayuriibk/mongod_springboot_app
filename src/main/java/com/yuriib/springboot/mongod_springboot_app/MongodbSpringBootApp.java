9b9a698f5f68a33086ff7c8ee5897afd9a344952
yuriybk <email2yurii@gmail.com>
2020-02-24 17:32:55 +0200

    minor

--- src/main/java/com/yuriib/springboot/mongod_springboot_app/MongodbSpringBootApp.java
+++ src/main/java/com/yuriib/springboot/mongod_springboot_app/MongodbSpringBootApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongodSpringbootAppApplication {
+public class MongodbSpringBootApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(MongodSpringbootAppApplication.class, args);
+        SpringApplication.run(MongodbSpringBootApp.class, args);
     }
 
 }
--- src/test/java/com/yuriib/springboot/mongod_springboot_app/MongodbSpringBootAppTests.java
+++ src/test/java/com/yuriib/springboot/mongod_springboot_app/MongodbSpringBootAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MongodSpringbootAppApplicationTests {
+class MongodbSpringBootAppTests {
 
     @Test
     void contextLoads() {
